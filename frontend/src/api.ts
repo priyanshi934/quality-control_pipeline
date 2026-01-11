e1aad6db21e2520862bd19e2495d15507f187d45
Priyanshi Grover <priyanshi.grover04@gmail.com>
2026-01-11 16:54:46 +0530

    Connect frontend to Render backend

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://quality-control-pipeline.onrender.com';
 
 export const api = axios.create({
   baseURL: API_URL,
